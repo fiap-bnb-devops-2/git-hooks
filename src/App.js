9467dd003a57f4666b92904ec683437423619075
Rafael Ribeiro <anthonyribeiro1910@gmail.com>
2024-09-24 11:30:08 -0300

    python

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aprenda Git Hooks
+          Aprenda Git Hooks e Python
         </a>
       </header>
     </div>
