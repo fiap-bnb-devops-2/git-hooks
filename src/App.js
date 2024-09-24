59f744308a8a8fa76620d4b0726f1c738351de71
Rafael Ribeiro <anthonyribeiro1910@gmail.com>
2024-09-24 11:16:03 -0300

    feat: learn text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprenda Git Hooks
         </a>
       </header>
     </div>
